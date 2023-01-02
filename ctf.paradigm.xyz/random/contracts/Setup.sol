3e5c8afd414c86bc82ba3a54690601bffc8ee3a7
easonnong <nongeason@gmail.com>
2023-01-02 10:42:47 +0800

    solved

--- ctf.paradigm.xyz/random/contracts/Setup.sol
+++ ctf.paradigm.xyz/random/contracts/Setup.sol
@@ -4,11 +4,17 @@ pragma solidity 0.8.15;
 
 import "./Random.sol";
 
+// ********************
+// I'm thinking of a number between 4 and 4
+// ********************
 contract Setup {
     Random public random;
 
     constructor() {
         random = new Random();
+        // **********
+        random.solve(4);
+        // **********
     }
 
     function isSolved() public view returns (bool) {
