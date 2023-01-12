f96caf34e5f8d72a4f02202ae294f6d00502389b
easonnong <nongeason@gmail.com>
2023-01-12 11:21:40 +0800

    BitMap add

--- note/ERC721TrustlessFilter/src/ERC721TrustlessFilter.sol
+++ note/ERC721TrustlessFilter/src/ERC721TrustlessFilter.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-abstract contract ERC721TrustlessFilter {
-    constructor() {}
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/BitMaps.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+abstract contract ERC721TrustlessFilter is Ownable, ERC721 {
+    using BitMaps for BitMaps.BitMap;
 }
