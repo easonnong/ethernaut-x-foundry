34c408bc57970ec1fd6227b76b94a96eba1baefc
easonnong <nongeason@gmail.com>
2023-03-20 08:58:05 +0800

    init

--- note/ERC721TrustlessFilter/test/Counter.t.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-import "forge-std/Test.sol";
-import "../src/ERC721TrustlessFilter.sol";
-
-contract ERC721TrustlessFilterTest is Test {}
